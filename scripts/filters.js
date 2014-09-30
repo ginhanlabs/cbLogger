65a999bfb18aeaeceed1ebaf024281b7a7cd0e8a
ginhanlabs <ginhanlabs@gmail.com>
2014-09-29 23:09:14 -0400

    filter totals

--- scripts/filters.js
+++ scripts/filters.js
@@ -7,4 +7,11 @@ cbApp.filter("displayTitles", function(){
            }
         }
     })
+    .filter("total", function() {
+        return function(items, field) {
+            var total = 0, i = 0;
+            for (i = 0; i < items.length; i++) total += items[i][field];
+            return total;
+        }
+    });
 
--- scripts/homeController.js
+++ scripts/homeController.js
@@ -6,35 +6,7 @@ cbApp.controller('homeController',['$scope', function($scope){
         $scope.totalTitleIssues = 0;
         $scope.displayLimit = 3;
 
-        $scope.$on("totalPublishers", function(evt, numOfItems){
-            $scope.totalIssuesByPublishers = numOfItems;
-        })
-
-        $scope.$on("totalIssuesPerTitle", function(evt, numOfItems){
-            $scope.totalTitleIssues = numOfItems;
-        })
  }])
-    .directive("tallyBook",  function(){
-       var numOfItems = 0;
-       var eId = '';
-        return {
-            restrict:"A",
-            scope: {
-                count :"=num",
-                emitterId:"="
-            },
-            link: function(scope, elem, attrs){
-                if (eId != scope.emitterId){
-                    eId = scope.emitterId;
-                    numOfItems = scope.count;
-                }
-                else {
-                    numOfItems += scope.count;
-                    scope.$emit(scope.emitterId, numOfItems );
-                }
-            }
-        }
-    })
 
 
 
