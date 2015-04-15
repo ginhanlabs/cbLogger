2267df06de6ac22afac4407a9109c6019a28fe00
ginhanlabs <ginhanlabs@gmail.com>
2015-04-15 17:37:44 -0400

    directive talking to directive

--- partials/newEntry/new-entry-directive.js
+++ partials/newEntry/new-entry-directive.js
@@ -1,54 +1,61 @@
 (function(){
     angular.module('cbApp').directive('newEntryDirective',   function(){
+    	var addEntry;
         return {
             restrict : "E",
             scope: {
                 titleslist: "=",
                 publisherlist: "="
             },
+            require: "^homedirective",
             templateUrl:'partials/newEntry/new-entry-template.html',
-            link: function(scope, elem, attrs){
+            link: function(scope, elem, attrs, homedirectiveCtrl){
+            	
+            	  addEntry = function(newEntry){
+            	  	homedirectiveCtrl.updateEntries(newEntry);
+            	  };
             },
 
             controller: function($scope){
                 $scope.setPublisher = function(val){
                     $scope.selectedPublisher = val.publisherId;
-                }
-
-              //  $scope.form = {};
-                 $scope.master = {
+                };
+				
+			  $scope.master = {
                     "publisher":  undefined,
                     "title": "",
                     "issue":  "",
                     "qty" :  "",
                     "price" :  "",
-                    "value":  "",
+                    "mkt":  "",
                     "condition":  "",
                     "box" :  "",
                     "notes": ""
                 };
 
+               
                 $scope.addEntry = function(){
-                    /*if (entryForm.$valid) {*/
-                        var newEntry = {
-                            "publisher" : {id:entryForm[0].value, name: entryForm[0].options[entryForm[0].selectedIndex].text},
-                            "title": {id: entryForm[1].value, name: entryForm[1].options[entryForm[1].selectedIndex].text},
-                            "issue":  entryForm[2].value,
-                            "qty" :  entryForm[3].value,
-                            "price" :  entryForm[4].value,
-                            "value":  entryForm[5].value,
-                            "condition":  entryForm[6].value,
-                            "box" :  entryForm[7].value,
-                            "notes" : entryForm[8].value
-                        };
-
-                        $scope.$emit("updateGridData", newEntry);
-                        angular.copy($scope.master,$scope.form);
-                    /*}*/
-
-                }
+                                 
+                   var newEntry = {
+                       "publisher" : {id:entryForm[0].value, name: entryForm[0].options[entryForm[0].selectedIndex].text},
+                       "title": {id: entryForm[1].value, name: entryForm[1].options[entryForm[1].selectedIndex].text},
+                       "issue":  entryForm[2].value,
+                       "qty" :  entryForm[3].value,
+                       "price" :  entryForm[4].value,
+                       "mkt":  entryForm[5].value,
+                       "condition":  entryForm[6].value,
+                       "box" :  entryForm[7].value,
+                       "notes" : entryForm[8].value
+                   };
+                    addEntry(newEntry);
+          		 //$scope.$emit("updateGridData", newEntry);
+                           angular.copy($scope.master,$scope.form);
+                      
+   
+                   };
+               
 
             }
-        }
-    })
+        };
+    });
 })();
